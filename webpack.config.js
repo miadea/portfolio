e3313d00e252f20b38cd21487004c80a38629c54
miadea <kitcat2675@gmail.com>
2017-04-11 19:32:52 -0400

    moved build to docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'build/public/');
+var BUILD_DIR = path.resolve(__dirname, 'docs/public/');
 var APP_DIR = path.resolve(__dirname, 'app/');
 
 var config = {
