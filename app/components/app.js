b149dbb00ada8c266aec726541295b472da663fc
miadea <kitcat2675@gmail.com>
2017-04-11 19:41:01 -0400

    moved to older router

--- app/components/app.js
+++ app/components/app.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router';
 import "../styles/index.less";
 import Home from "./home";
 
 export default class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/gallery' component={Gallery} />
-        </div>
-      </BrowserRouter>
+      <Router history={browserHistory}>
+        <Route path='/' component={Home} />
+        <Route path='/gallery' component={Gallery} />
+      </Router>
     );
   }
 }
