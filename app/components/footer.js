6306284e176396e40ea04362ec4aadf585ca0c6d
Emma Stoumen <emmastoumen@Emmas-MacBook-Pro.local>
2018-11-13 15:33:10 -0500

    copyright bump

--- app/components/footer.js
+++ app/components/footer.js
@@ -4,7 +4,7 @@ export default class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        Copyright © 2017 Copyright Holder All Rights Reserved.
+        Copyright © 2018 Copyright Holder All Rights Reserved.
       </div>
     );
   }
